ed92fb6112df9362f4095b4544efa15a12da4881
willhorton <willhorton@Zips-MBP-4.home>
2016-01-26 20:02:44 -0500

    Modified Wax constructor to take human input and validate with regex

--- src/main/java/ski_wax_selection/Wax.java
+++ src/main/java/ski_wax_selection/Wax.java
@@ -1,5 +1,8 @@
 package ski_wax_selection;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Created by willhorton on 1/25/16.
  */
@@ -8,7 +11,32 @@ public class Wax {
     private WaxColor color;
     private WaxGrade grade;
 
-    public Wax(int temp, String snowConditions) {
+    public Wax() {
+
+        int temp = 0;
+        String snowConditions = null;
+        Scanner sc = new Scanner(System.in);
+
+        // Get and validate temp input
+        while(true) {
+            System.out.println("Please enter a temperature:");
+            String tempString = sc.nextLine();
+            if(tempString.matches("-?\\d+")) {
+                temp = Integer.parseInt(tempString);
+                break;
+            }
+            System.out.println("Please enter numbers only");
+        }
+
+        // Get and validate snow conditions input
+        while(true) {
+            System.out.println("Please enter the snow conditions ('firm', 'crusty', or 'powder'):");
+            snowConditions = sc.nextLine();
+            if(snowConditions.matches("^(firm)|(crusty)|(powder)$")) {
+                break;
+            }
+            System.out.println("Invalid snow type: please enter 'firm', 'crusty', or 'powder'.");
+        }
 
         this.color = WaxColor.determineWaxColor(temp);
         this.grade = WaxGrade.determineWaxGrade(snowConditions);
@@ -16,20 +44,13 @@ public class Wax {
     }
 
     public String displayWax() {
-        return "Wax color is " + color + ", wax grade is " + grade;
+        return "Recommended wax type for these conditions is: \nColor: " + color + ", Grade: " + grade;
     }
 
     public static void main(String[] args) {
 
-        // Demonstrate getting and displaying different types of waxes
-        Wax skiWax = new Wax(23, "firm");
+        Wax skiWax = new Wax();
         System.out.println(skiWax.displayWax());
-
-        Wax skiWax1 = new Wax(5, "crusty");
-        System.out.println(skiWax1.displayWax());
-
-        Wax skiWax2 = new Wax(31, "powder");
-        System.out.println(skiWax2.displayWax());
 
     }
 
